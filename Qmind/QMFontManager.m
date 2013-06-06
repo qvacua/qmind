97afe4641f91423226211c0afe641244d3fac661
Tae Won Ha <qvacua@gmail.com>
2013-06-06 21:11:55 +0200

    #10 _XYZ -> self.XYZ

--- Qmind/QMFontManager.m
+++ Qmind/QMFontManager.m
@@ -20,9 +20,13 @@ static NSString *const qDefaultSansSerifFontName = @"SansSerif";
 static NSString *const qDefaultSerifFondName = @"Serif";
 static NSString *const qTimesFontName = @"Times";
 
-@implementation QMFontManager {
-    NSFont *_defaultFont;
-}
+@interface QMFontManager ()
+
+@property (readonly) NSFont *defaultFont;
+
+@end
+
+@implementation QMFontManager
 
 TB_AUTOWIRE(settings)
 TB_AUTOWIRE(fontManager)
@@ -37,7 +41,7 @@ TB_AUTOWIRE(fontManager)
     CGFloat fontSize = fontSizeObj.floatValue;
 
     if (fontName == nil || [fontName isEqualToString:qDefaultSansSerifFontName]) {
-        fontName = _defaultFont.familyName;
+        fontName = self.defaultFont.familyName;
     }
 
     if ([fontName isEqualToString:qDefaultSerifFondName]) {
@@ -45,48 +49,48 @@ TB_AUTOWIRE(fontManager)
     }
 
     if (fontSizeObj == nil) {
-        fontSize = [_defaultFont pointSize];
+        fontSize = [self.defaultFont pointSize];
     }
 
     NSFont *font = [NSFont fontWithName:fontName size:fontSize];
     if (font == nil) {
         log4Debug(@"Unknown font '%@' encountered, falling back to default font", fontName);
-        font = [NSFont fontWithName:[_defaultFont familyName] size:fontSize];
+        font = [NSFont fontWithName:[self.defaultFont familyName] size:fontSize];
     }
 
     if (boldObj != nil) {
-        font = [_fontManager convertFont:font toHaveTrait:NSBoldFontMask];
+        font = [self.fontManager convertFont:font toHaveTrait:NSBoldFontMask];
     }
 
     if (italicObj != nil) {
-        font = [_fontManager convertFont:font toHaveTrait:NSItalicFontMask];
+        font = [self.fontManager convertFont:font toHaveTrait:NSItalicFontMask];
     }
 
     return font;
 }
 
 - (NSDictionary *)fontAttrDictFromFont:(NSFont *)font {
-    if ([font isEqual:_defaultFont]) {
+    if ([font isEqual:self.defaultFont]) {
         return nil;
     }
 
     NSMutableDictionary *attrDict = [[NSMutableDictionary allocWithZone:nil] initWithCapacity:4];
 
     NSString *fontName = font.familyName;
-    if ([fontName isEqualToString:[_defaultFont familyName]] == NO) {
+    if ([fontName isEqualToString:[self.defaultFont familyName]] == NO) {
         [attrDict setObject:fontName forKey:qNameKey];
     }
 
     NSInteger fontSize = (NSInteger) font.pointSize;
-    if ([_fontManager traitsOfFont:font] & NSFontBoldTrait) {
+    if ([self.fontManager traitsOfFont:font] & NSFontBoldTrait) {
         [attrDict setObject:qTrueValue forKey:qBoldKey];
     }
 
-    if ([_fontManager traitsOfFont:font] & NSFontItalicTrait) {
+    if ([self.fontManager traitsOfFont:font] & NSFontItalicTrait) {
         [attrDict setObject:qTrueValue forKey:qItalicKey];
     }
 
-    if (fontSize != [_defaultFont pointSize] || [attrDict count] > 0) {
+    if (fontSize != [self.defaultFont pointSize] || [attrDict count] > 0) {
         [attrDict setObject:[NSString stringWithFormat:@"%li", fontSize] forKey:qSizeKey];
     }
 
@@ -95,7 +99,7 @@ TB_AUTOWIRE(fontManager)
 
 #pragma mark TBInitializingBean
 - (void)postConstruct {
-    _defaultFont = [_settings settingForKey:qSettingDefaultFont];
+    _defaultFont = [self.settings settingForKey:qSettingDefaultFont];
 
     NSString *fontPath = [[NSBundle bundleForClass:self.class] pathForResource:@"fontawesome-webfont" ofType:@"ttf"];
     NSData *fontData = [[NSData alloc] initWithContentsOfFile:fontPath];
@@ -104,6 +108,7 @@ TB_AUTOWIRE(fontManager)
     CGFontRef cgFont = CGFontCreateWithDataProvider(fontDataProvider);
     CGDataProviderRelease(fontDataProvider);
 
+    // if we pass NULL for attributes, we crash when releasing the font descriptor
     CTFontDescriptorRef fontDescriptor = CTFontDescriptorCreateWithAttributes((__bridge CFDictionaryRef) @{});
     CTFontRef ctFont = CTFontCreateWithGraphicsFont(cgFont, 0, NULL, fontDescriptor);
     CFRelease(fontDescriptor);
