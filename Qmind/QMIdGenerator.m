ebb3101bb37e522a53e5cc464e93527d381be091
Tae Won Ha <qvacua@gmail.com>
2014-01-12 12:17:42 +0100

    #10 minor reformatting

--- Qmind/QMIdGenerator.m
+++ Qmind/QMIdGenerator.m
@@ -9,22 +9,23 @@
 #import <TBCacao/TBCacao.h>
 #import "QMIdGenerator.h"
 
+
 @implementation QMIdGenerator
 
 #pragma mark Public
 - (NSString *)nodeId {
-    return [NSString stringWithFormat:@"ID_%@", [self uuid]];
+  return [@"ID_" stringByAppendingString:self.uuid];
 }
 
 #pragma mark Private
 - (NSString *)uuid {
-    CFUUIDRef cfUuid = CFUUIDCreate(NULL);
+  CFUUIDRef cfUuid = CFUUIDCreate(NULL);
 
-    CFStringRef cfUuidStr = CFUUIDCreateString(NULL, cfUuid);
-    CFRelease(cfUuid);
+  CFStringRef cfUuidStr = CFUUIDCreateString(NULL, cfUuid);
+  CFRelease(cfUuid);
 
-    NSString *result = (__bridge_transfer NSString *) cfUuidStr;
-    return result;
+  NSString *result = (__bridge_transfer NSString *) cfUuidStr;
+  return result;
 }
 
 @end
